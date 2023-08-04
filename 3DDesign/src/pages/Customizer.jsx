495b63ac1578287783ebca4fa815f2a35a3c41d9
Prem Prakash Shinde <shindeprem102@gmail.com>
2023-08-04 21:37:19 +0530

    ai generated picture usingopenai

--- 3DDesign/src/pages/Customizer.jsx
+++ 3DDesign/src/pages/Customizer.jsx
@@ -37,9 +37,27 @@ const Customizer = () => {
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
-        return <AIPicker />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
+    }
+  };
+  const handleSubmit = async (type) => {
+    if (!prompt) return "Please enter a Prompt";
+    try {
+      // call out backend to generate an ai image
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
     }
   };
 
